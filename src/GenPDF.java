16b2a392bf850c40232bbad90dc1e511b8191403
N1gorena <guangore@usc.edu>
2016-10-15 16:27:34 -0700

    Pushing laptop work

--- src/GenPDF.java
+++ src/GenPDF.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,6 +18,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.mysql.jdbc.PreparedStatement;
@@ -23,6 +27,9 @@ import com.mysql.jdbc.PreparedStatement;
 public class GenPDF extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String serverURL = "jdbc:mysql://localhost:3306/capstonedb?useSSL=false";
+	//TODO finish Statement
+	private final String query = "SELECT field_name,type,field_option FROM pdf_structure where PdfID = ?";
+	private Map<String,FieldElement> fieldData;
    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,28 +43,55 @@ public class GenPDF extends HttpServlet {
 		String message = "None";
 		JSONObject obj = new JSONObject();
 		ResultSet results = null;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			dbConn = DriverManager.getConnection(serverURL,"root","Trojans17");
-			StringBuilder newStringBuilder = new StringBuilder();
-			newStringBuilder.append("Connected to db. Keys<");
-			Map<String,String[]> requestKeyVals = request.getParameterMap();
-			Set<String> keySet = requestKeyVals.keySet();
-			for(String s : keySet){
-				newStringBuilder.append(s+",");
+		fieldData = new HashMap<>();
+		if(request.getParameter("PdfID") != null){
+			try {
+				//Recieving key PdfID
+				Class.forName("com.mysql.jdbc.Driver");
+				dbConn = DriverManager.getConnection(serverURL,"root","Trojans17");
+				Integer id = Integer.valueOf(request.getParameter("PdfID") );
+				
+				genStatement =  (PreparedStatement) dbConn.prepareStatement(query);
+				genStatement.setInt(1, id);
+				results = genStatement.executeQuery();
+				while(results.next()){
+					FieldElement tempFieldElement;
+					String nombre = results.getString("field_name");
+					String tipo = results.getString("type");
+					String val = results.getString("field_option");
+					if(fieldData.containsKey(nombre)){
+						tempFieldElement = fieldData.get(nombre);
+					}
+					else{
+						tempFieldElement = new FieldElement(nombre, tipo);
+					}
+					tempFieldElement.addValue(val);
+					fieldData.put(nombre, tempFieldElement);
+				}
+				
+				Iterator<String> sI = fieldData.keySet().iterator();
+				while(sI.hasNext()){
+					FieldElement f = fieldData.get(sI.next());
+					JSONObject fieldEle = new JSONObject();
+					JSONArray fieldOptions = new JSONArray();
+					fieldEle.put("type", f.mType);
+					fieldOptions.addAll(f.mValues);
+					fieldEle.put("value", fieldOptions);
+					obj.put(f.mName, fieldEle);
+					
+				}
+				obj.put("Message", "Success");
+				obj.put("Success", true);
+					
+			} catch (SQLException e) {
+				obj.put("Message", e.getMessage());
+				obj.put("Success", false);
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				obj.put("Message", e.getMessage());
+				obj.put("Success", false);
+				e.printStackTrace();
 			}
-			newStringBuilder.insert(newStringBuilder.lastIndexOf(","),">");
-			obj.put("Keys", newStringBuilder.toString());
-			//dbConn.prepareStatement("SELECT ");
-		} catch (SQLException e) {
-			obj.put("Message", e.getMessage());
-			obj.put("Success", false);
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			obj.put("Message", e.getMessage());
-			obj.put("Success", false);
-			e.printStackTrace();
 		}
 	}
 
@@ -67,6 +101,20 @@ public class GenPDF extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	private class FieldElement{
+		String mName;
+		String mType;
+		ArrayList<String> mValues;
+		public FieldElement(String fieldName,String fieldType){
+			mName = fieldName;
+			mType = fieldType;
+			mValues = new ArrayList<>();
+		}
+		public void addValue(String val){
+			mValues.add(val);
+		}
 	}
 
 }
